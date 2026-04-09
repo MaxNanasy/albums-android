af02a6d7aa73f0ddf4221a40d207ea638a3932d4
Max Nanasy <max.nanasy@gmail.com>
2026-04-09 11:27:32 -0700

    [WIP] resolveAllDependencies task

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,20 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
+
+allprojects {
+    tasks.register("resolveAllDependencies") {
+        doLast {
+            configurations
+                .filter { it.isCanBeResolved }
+                .filter { cfg ->
+                    (cfg.name.endsWith("CompileClasspath") || cfg.name.endsWith("RuntimeClasspath")) &&
+                    !cfg.name.endsWith("TestCompileClasspath")
+                }
+                .forEach { cfg ->
+                    println("Resolving $path:${cfg.name}")
+                    cfg.incoming.files.files
+                }
+        }
+    }
+}
