d164eb7ddff6bbdbe69c2a1cc6969abc806ef652
Max Nanasy <max.nanasy@gmail.com>
2026-04-21 14:43:46 -0700

    Add comment to GSON dependency indicating that it's needed by spotify-app-remote lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,7 +94,7 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation(files("libs/spotify-app-remote-release-0.8.0.aar"))
-    implementation("com.google.code.gson:gson:2.13.2")
+    implementation("com.google.code.gson:gson:2.13.2") // Dependency of libs/spotify-app-remote-*.aar
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:core-ktx:1.6.1")
